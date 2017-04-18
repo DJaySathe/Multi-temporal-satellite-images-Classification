707aa1dda66cc55c7c29c233ea3fb34e09cf7e4c
Ankur Garg <agarg12@ncsu.edu>
2017-04-17 21:59:01 -0400

    Updated MLC file python

--- PythonCode/classifierMLC.py
+++ PythonCode/classifierMLC.py
@@ -1,43 +1,46 @@
 import pandas as pd
 from numpy.random import *
+import numpy as np
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 
 
 # Training  and Accuracy on 'ValidationData'
 
-data = pd.read_csv('../TrainingData/ValidationData/ValidationData-2015-04-19.csv')
+data = pd.read_csv('../Data/Training/ValidationDataImage1.csv')
 cols = data.columns
 
 data.drop(cols[0], axis=1, inplace=True)
-
+cols = data.columns
 b = choice(range(len(data)), len(data))
 
-data = data.iloc[b].reset_index()
+data = data.iloc[b].reset_index(drop=True)
 testIdx = np.random.rand(len(data)) < 0.2
 
-testData = data.iloc[testIdx].reset_index()
-trainData = data[~testIdx].reset_index()
+testData = data.iloc[testIdx].reset_index(drop=True)
+trainData = data[~testIdx].reset_index(drop=True)
 
 cols = trainData.columns
 trainData.drop(cols[[0,1]], axis=1, inplace=True)
 testData.drop(cols[[0,1]], axis=1, inplace=True)
 
 cols = trainData.columns
-counts = trainData['Class'].value_counts()
+counts = trainData['Class'].value_counts(sort=False)
 priorProb = counts/len(trainData)
 
-image1MLCmodel = MultinomialNB(class_prior = priorProb)
-image1MLCmodel.fit(trainData[cols[1:]], trainData['Class'])
+image1MLCmodel = MultinomialNB(class_prior = np.array(priorProb))
+X = trainData[cols[1:]]
+y = trainData['Class']
+image1MLCmodel.fit(X, y)
 predictions = image1MLCmodel.predict(testData[cols[1:]])
 image1MLCmodel.score(testData[cols[1:]], testData['Class'])
 
 
 # Accuracy on 'AccuracyTestData'
 
-accuracyTestData = pd.read_csv("../TrainingData/AccuracyTestingData/AccuracyData-2015-12-31.csv")
+accuracyTestData = pd.read_csv("../Data/Testing/AccuracyDataImage1.csv")
 cols = accuracyTestData.columns
 accuracyTestData.drop(cols[[0,1,2]], axis=1, inplace=True)
-accuracyTestData = accuracyTestData.reset_index()
+accuracyTestData = accuracyTestData.reset_index(drop=True)
 cols = accuracyTestData.columns
 predictions = image1MLCmodel.predict(accuracyTestData[cols[1:]])
 image1MLCmodel.score(accuracyTestData[cols[1:]], accuracyTestData['Class'])
